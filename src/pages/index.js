eea62f25cd65ad68245b17abf407b440ec896849
Hannes Koehler <hannesk@hanneskoehler.eu>
2020-11-28 19:49:36 +0100

    wf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div><p>Hello Azure-World from Gatsby and GitHub!</p><p><img src="https://github.com/HannesKoeh/gatsby-test/workflows/Azure%20Static%20Web%20Apps%20CI/CD/badge.svg?branch=main"/></p></div>
+  return <div><p>Hello Azure-World from Gatsby and GitHub! workflows</p><p><img src="https://github.com/HannesKoeh/gatsby-test/workflows/Azure%20Static%20Web%20Apps%20CI/CD/badge.svg?branch=main"/></p></div>
 }
