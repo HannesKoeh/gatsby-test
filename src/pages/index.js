593bc17295ff5148a99433f37431ebaccd8ea97c
HannesKoeh <46895123+HannesKoeh@users.noreply.github.com>
2020-12-20 13:04:20 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,6 @@ class Homepage extends Component {
     const data = this.props.data
 
     return (
-      <>
-        {/* <div>
-          <h1>Pages</h1>
-          {data.allWordpressPage.edges.map(({ node }) => (
-            <div key={node.slug}>
-              <Link to={node.slug}>
-                <h2>{node.title}</h2>
-              </Link>
-              <h3>{node.excerpt}</h3>
-            </div>
-          ))}
-        </div> */}
-
         <h1>Posts</h1>
         {data.allWordpressPost.edges.map(({ node }) => (
           <div key={node.slug}>
